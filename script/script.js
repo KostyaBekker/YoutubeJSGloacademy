1e7144d8a0676452287a4f397c8170954039b8c8
KostyaBekker <bekker_kostya@ukr.net>
2019-10-10 21:37:52 +0300

    commit

--- script/script.js
+++ script/script.js
@@ -10,64 +10,156 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const toggleKeyboard = () => keyboard.style.top = keyboard.style.top ? '' : '50%';
 
+    const changeLang = (btn, lang) => {
+      const langRu = ['ë', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', '⬅',
+        'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ',
+        'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э',
+        'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/',
+        'en', ''
+      ];
+      const langEn = ['§', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', '⬅',
+        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',
+        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"',
+        'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
+        'ru', ''
+      ];
+
+      if (lang === 'en') {
+          btn.forEach((elem, i) =>{
+            elem.textContent = langEn[i];
+          })
+        } else {
+          btn.forEach((elem, i) =>{
+            elem.textContent = langRu[i];
+          })
+        }
+      }
+    
+
     const typing = event => {
       const target = event.target;
 
       if (target.tagName === 'BUTTON') {
-        searchInput.value += target.textContent.trim();
-      }
-      //backspace
-      //space
-    }
+        const contentButton = target.textContent.trim();
+        const buttons = [...keyboard.querySelectorAll('button')]
 
-    keyboardButton.addEventListener('click', toggleKeyboard); 
+          .filter(elem => elem.style.visibility !== 'hidden');
+        if (contentButton === '⬅') {
+          searchInput.value = searchInput.value.slice(0, length - 1);
+        } else if (!contentButton) {
+          searchInput.value += ' ';
+        } else if (contentButton === 'en' || contentButton === 'ru') {
+          changeLang(buttons, contentButton);
+        } else {
+          searchInput.value += contentButton;
+        }
+
+      }
+
+    }
+    keyboardButton.addEventListener('click', toggleKeyboard);
     closeKeyboard.addEventListener('click', toggleKeyboard);
     keyboard.addEventListener('click', typing);
   }
   //menu
   {
-  const burger = document.querySelector('.spinner');
-  const sidebarMenu = document.querySelector('.sidebarMenu');
+    const burger = document.querySelector('.spinner');
+    const sidebarMenu = document.querySelector('.sidebarMenu');
 
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    sidebarMenu.classList.toggle('rollUp');
-  });
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('active');
+      sidebarMenu.classList.toggle('rollUp');
+    });
 
-  sidebarMenu.addEventListener('click', e => {
-    let target = e.target;
-    
-    target = target.closest('a[href="#"]');
+    sidebarMenu.addEventListener('click', e => {
+      let target = e.target;
 
-    if(target) {
-      const parentTarget = target.parentNode;
-      sidebarMenu.querySelectorAll('li').forEach(elem => {
-        
-        
-        if (elem === parentTarget) {
-          elem.classList.add('active');
-        } else {
-          elem.classList.remove('active');
-        }
-      
+      target = target.closest('a[href="#"]');
+
+      if (target) {
+        const parentTarget = target.parentNode;
+        sidebarMenu.querySelectorAll('li').forEach(elem => {
+
+
+          if (elem === parentTarget) {
+            elem.classList.add('active');
+          } else {
+            elem.classList.remove('active');
+          }
+
+        })
+      }
     })
-  }
-  })
 
   }
 
   //Модальное окно
   {
-    const divYouttuber = documet.createElement('div');
+
+   
+
+    document.body.insertAdjacentHTML('beforeend', `
+      <div class="youTuberModal">
+      <div id="youtuberClose">&#215;</div>
+      <div id="youtuberContainer"></div>
+      </div>
+    `);
+
+    const youtuberItems = document.querySelectorAll('[data-youtuber]');
+    const youTuberModal = document.querySelector('.youTuberModal');
+    const youtuberContainer = document.getElementById('youtuberContainer');
+
+    const qw = [3840, 2560, 1920, 1280, 854, 640, 426, 256];
+    const qh = [2160, 1440, 1080, 720, 480, 360, 240, 144];
+
+    const sizeVideo = () => {
+      let ww = document.documentElement.clientWidth;
+      let wh = document.documentElement.clientHeight;
+
+        for (let i = 0; i < qw.length; i++) {
+          if( ww > qw[i]) {
+            youtuberContainer.querySelector('iframe').style.cssText = `
+            width: ${qw[i]}px;
+            height: ${qh[i]}px;
+            `;
+            youtuberContainer.style.cssText = `
+            width: ${qw[i]}px;
+            height: ${qh[i]}px;
+            top: ${(wh - qh[i]) /2}px;
+            left:${(ww - qw[i]) /2}px;
+            `;
+            break;
+          }
+        }
+
+    }
+
+    youtuberItems.forEach(elem => {
+      elem.addEventListener('click', () => {
+        const idVideo = elem.dataset.youtuber;
+        youTuberModal.style.display = 'block';
+
+        const youTuberFrame = document.createElement('iframe');
+        youTuberFrame.src = `https://youtube.com/embed/${idVideo}`;
+        youtuberContainer.insertAdjacentElement('beforeend', youTuberFrame); 
+
+        window.addEventListener('resize', sizeVideo); 
+        sizeVideo();
+
+      })
+    })
+
+    youTuberModal.addEventListener('click', () => {
+      youTuberModal.style.display = '';
+      youtuberContainer.textContent = '';
+      window.remove('resize', sizeVideo); 
+    });
 
     
   }
-
-
-
-
-
-
-
+  {
+    const API_KEY = 'AIzaSyCuxQSWMj5eXFFCWTQHIuDJtquH7ciuFz4';
+    const CLIENT_ID = '341464842826-8qsr2ol37ko2tcb4job2hva4o3sk6jim.apps.googleusercontent.com';
+  }
 
 });
